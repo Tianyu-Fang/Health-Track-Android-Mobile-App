c8b9fd30deee9d697ca069111f8191824e7ca6a7
lilacwine <2846135079@qq.com>
2022-11-20 21:24:37 +0800

    profile

--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -2,10 +2,59 @@ package com.example.assignment2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.profile)
+
+        constructRecyclerView(this@MainActivity)
     }
+}
+
+fun constructRecyclerView(context: AppCompatActivity) {
+    val ProfileAdapter = ProfileAdapter(populateCourseList())
+
+    val courseRecyclerView = context.findViewById<RecyclerView>(R.id.profile_recyclerview)
+
+    val linearLayoutManager = LinearLayoutManager(
+        context,
+        LinearLayoutManager.VERTICAL,
+        false
+    )
+
+//    val gridLayoutManager = GridLayoutManager(
+//        context,
+//        2,
+//        RecyclerView.VERTICAL,
+//        false
+//    )
+    courseRecyclerView.layoutManager = linearLayoutManager
+    courseRecyclerView.adapter = ProfileAdapter
+}
+
+fun populateCourseList(): ArrayList<ProfileModel> {
+    val courseList = ArrayList<ProfileModel>()
+    courseList.add(
+        ProfileModel(1, "PE3", "Good subject")
+    )
+    courseList.add(
+        ProfileModel(2, "IoT", "Very good subject")
+    )
+    courseList.add(
+        ProfileModel(3, "PE4", "Good subject")
+    )
+    courseList.add(
+        ProfileModel(4, "IoT2", "Very good subject")
+    )
+    courseList.add(
+        ProfileModel(5, "PE5", "Good subject")
+    )
+    courseList.add(
+        ProfileModel(6, "IoT3", "Very good subject")
+    )
+
+    return courseList
 }
---
+++ app/src/main/java/com/example/assignment2/ProfileModel.kt
@@ -0,0 +1,7 @@
+package com.example.assignment2
+
+data class ProfileModel(
+    val id: Int = 0,
+    val title: String?= null,
+    val data: String?= null
+)
