2be049c06b85c9ccca0d9426ebeebe486fb8e3f8
BrokebackDrug <fangtianyu0208@gmail.com>
2022-12-01 14:53:02 +0800

    info pager setup

--- app/src/main/java/com/example/assignment2/info/InfoActivity.kt
+++ app/src/main/java/com/example/assignment2/info/InfoActivity.kt
@@ -1,8 +1,12 @@
 package com.example.assignment2.info
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.widget.Button
+import androidx.appcompat.view.menu.MenuBuilder
+import androidx.core.view.MenuCompat
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.example.assignment2.R
@@ -20,6 +24,21 @@ class InfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
         bindView()
+        addFragmentToList()
+        setupViewPager()
+    }
+    @SuppressLint("RestrictedApi")
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_options, menu)
+
+        // Show icons on Overflow option menu
+        if (menu is MenuBuilder) {
+            menu.setOptionalIconsVisible(true)
+        }
+
+        MenuCompat.setGroupDividerEnabled(menu!!, true)
+
+        return super.onCreateOptionsMenu(menu)
     }
 
     private fun setupViewPager() {
