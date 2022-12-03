e945ef52e69bc3c72e5eddca09b46a368f16bfb2
Dracilem <75916498+Dracilem@users.noreply.github.com>
2022-12-03 11:35:57 +0800

    Composition UI
    Finish dashboard
    add checkin navigation

---
+++ app/src/main/java/com/example/assignment2/CheckinBoardFragment.kt
@@ -0,0 +1,39 @@
+package com.example.assignment2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+
+
+class CheckinBoardFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_checkin_board, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        val btnCheckin = view.findViewById<Button>(R.id.checkin_button)
+
+        btnCheckin.setOnClickListener {
+
+            view.findNavController().navigate(R.id.fillCheckinFragment2)
+
+        }
+    }
+
+
+}
