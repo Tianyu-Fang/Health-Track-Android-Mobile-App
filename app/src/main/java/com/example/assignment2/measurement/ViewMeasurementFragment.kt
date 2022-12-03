43b7c984c402bdeabdab113ffe8095f377a4a3f9
BrokebackDrug <fangtianyu0208@gmail.com>
2022-12-03 15:58:57 +0800

    update UI

--- app/src/main/java/com/example/assignment2/measurement/ViewMeasurementFragment.kt
+++ app/src/main/java/com/example/assignment2/measurement/ViewMeasurementFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.findNavController
 import com.example.assignment2.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -57,4 +60,32 @@ class ViewMeasurementFragment : Fragment() {
                 }
             }
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //health score
+        val btnHealthScore = view.findViewById<Button>(R.id.total_score_btn)
+
+        btnHealthScore.setOnClickListener {
+            view.findNavController().navigate(R.id.healthScoreFragment)
+        }
+
+
+        //height
+        val textHeight = view.findViewById<TextView>(R.id.height_data)
+
+        textHeight.setOnClickListener {
+            view.findNavController().navigate(R.id.heightFragment2)
+        }
+
+//        weight
+
+        val textWeight = view.findViewById<TextView>(R.id.weight_data)
+
+        textWeight.setOnClickListener {
+            view.findNavController().navigate(R.id.weightFragment2)
+        }
+    }
+
 }
