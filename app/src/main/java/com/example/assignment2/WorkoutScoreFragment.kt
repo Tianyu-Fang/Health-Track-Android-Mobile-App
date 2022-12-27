cc880aab5d85795a725848428f7204eaff2baca0
Dracilem <75916498+Dracilem@users.noreply.github.com>
2022-12-27 22:05:56 +0800

    workout score
    get the value from cloud and assign it to the textview

--- app/src/main/java/com/example/assignment2/WorkoutScoreFragment.kt
+++ app/src/main/java/com/example/assignment2/WorkoutScoreFragment.kt
@@ -1,15 +1,20 @@
 package com.example.assignment2
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.assignment2.databinding.FragmentWorkoutScoreBinding
+import com.example.assignment2.model.Workout
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class WorkoutScoreFragment: Fragment() {
 
     private lateinit var binding: FragmentWorkoutScoreBinding
+
+    private var db = Firebase.firestore
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +27,28 @@ class WorkoutScoreFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val workoutData = Workout(1000, 200, 300,
+            200)
+        val workoutDB = db.collection("Workout").document("randomnumber")
+            workoutDB.set(workoutData)
+        var data = Workout(1,1,1,1)
+
+            workoutDB.get().addOnSuccessListener { document ->
+                    data =
+                    Workout(
+//                            document.id
+                        (document.data!!["goal"] as Number?)!!.toInt(),
+                        (document.data!!["running_time"] as Number?)!!.toInt(),
+                        (document.data!!["walking_time"] as Number?)!!.toInt(),
+                        (document.data!!["other_time"] as Number?)!!.toInt()
+                    )
+                binding.walkingTV.text = "Walking: ${data.walking_time} minutes"
+                binding.runningTV.text = "Running: ${data.running_time} minutes"
+                binding.otherTV.text = "Other: ${data.other_time} minutes"
+            }
+//        binding.runningTV.text = "${data.running_time} minutes"
+//        binding.otherTV.text = "${data.other_time} minutes"
 
     }
 
---
+++ app/src/main/java/com/example/assignment2/model/Workout.kt
@@ -0,0 +1,15 @@
+package com.example.assignment2.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+class Workout(
+    var goal: Int = 0,
+
+    var walking_time: Int = 0,
+
+    var running_time: Int = 0,
+
+    var other_time: Int = 0
+): Parcelable
